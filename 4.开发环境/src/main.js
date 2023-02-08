d23ac967f82794dc92f892075c80da8920455fd6
angryWhy <875978822@qq.com>
2023-02-08 13:47:12 +0800

    开发环境

---
+++ 4.开发环境/src/main.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import print from './print';
+import "./style.css"
+import Icon from "./img.png"
+function component() {
+    const element = document.createElement('div');
+
+    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack !!!'], ' ');
+    element.classList.add('font-color')
+    const btn = document.createElement('button')
+    btn.innerHTML = 'button'
+    btn.onclick = print
+    const img = new Image()
+    img.src = Icon    
+    element.appendChild(img);
+    element.appendChild(btn);
+    return element;
+}
+
+document.body.appendChild(component());
