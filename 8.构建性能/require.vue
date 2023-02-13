764f3ed22a973c87fc8977470403279579045e27
angryWhy <875978822@qq.com>
2023-02-13 13:49:31 +0800

    8.构建性能

---
+++ 8.构建性能/require.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+}
+//require.context() 函数来创建自己的 context。
+//参数1：要搜索的目录。
+//参数2：布尔值是否要搜索其子目录
+//参数3：文件匹配的正则式
+const requ = require.context('../7.环境变量',true,/\js$/ig)
+</script>
+
+<style>
+
+</style>
